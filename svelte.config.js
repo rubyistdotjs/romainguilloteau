4b675c5dc3c3549f9c284ddafcc4043a6e790cba
rubyistdotjs <rubyistdotjs@gmail.com>
2023-05-10 18:05:10 +0200

    Cleanup config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,10 @@
 import adapter from "@sveltejs/adapter-netlify";
-import sveltePreprocess from "svelte-preprocess";
-import { preprocess } from "svelte/compiler";
-
-/**
- * @typedef {import("svelte/types/compiler/preprocess").PreprocessorGroup} PreprocessorGroup
- * @param {PreprocessorGroup[]} preprocessors
- * @returns {PreprocessorGroup[]}
- */
-function sequence(preprocessors) {
-  return preprocessors.map((preprocessor) => ({
-    markup({ content, filename }) {
-      return preprocess(content, preprocessor, { filename });
-    },
-  }));
-}
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sequence([sveltePreprocess({ preserve: ["ld+json"] })]),
-
-  kit: {
-    adapter: adapter(),
-  },
+  preprocess: [vitePreprocess({ preserve: ["ld+json"] })],
+  kit: { adapter: adapter() },
 };
 
 export default config;
