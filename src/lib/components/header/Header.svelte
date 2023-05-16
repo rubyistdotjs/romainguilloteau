bb5831ee21d4ca483dc76290a052970273bbf545
rubyistdotjs <rubyistdotjs@gmail.com>
2023-05-16 17:26:29 +0200

    Remove "photo" in an image alt

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -7,7 +7,7 @@
   class="container mx-auto px-8 py-5 flex flex-row justify-between items-center"
 >
   <div class="w-10 h-10 mr-10 rounded-full grayscale overflow-hidden">
-    <img src={mugshotSrc} alt="Close-up photo of Romain Guilloteau" />
+    <img src={mugshotSrc} alt="Close-up of Romain Guilloteau" />
   </div>
   <div>
     <DarkModeButton />
