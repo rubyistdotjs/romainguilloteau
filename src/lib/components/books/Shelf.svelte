32861f2f89d2bce27fb38d265045e3e76961faab
rubyistdotjs <rubyistdotjs@gmail.com>
2023-05-16 17:25:46 +0200

    Add link to goodreads profile

--- src/lib/components/books/Shelf.svelte
+++ src/lib/components/books/Shelf.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { PUBLIC_GOODREADS_USER_ID } from "$env/static/public";
-  import ExternalLinkIcon from "svelte-feather-icons/src/icons/ExternalLinkIcon.svelte";
   import Book from "./Book.svelte";
 
   export let shelfName: string;
@@ -9,7 +7,6 @@
 
   $: title = shelfTitle(shelfName);
   $: booksOverflow = computeBooksOverflow(booksCount, books.length);
-  $: url = shelfUrl(shelfName);
 
   function shelfTitle(name: string) {
     return name === "currently-reading"
@@ -21,10 +18,6 @@
 
   function computeBooksOverflow(total: string, displayed: number) {
     return parseInt(total, 10) - displayed;
-  }
-
-  function shelfUrl(name: string) {
-    return `https://www.goodreads.com/review/list/${PUBLIC_GOODREADS_USER_ID}?shelf=${name}&per_page=100`;
   }
 </script>
 
@@ -38,14 +31,6 @@
   {/each}
 
   {#if booksOverflow > 0}
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      class="inline-block text-gray-500 dark:text-gray-400 text-sm hover:text-indigo-600 dark:hover:text-indigo-500 mt-3"
-    >
-      ... and {booksOverflow} more
-      <ExternalLinkIcon size="13" class="inline-block align-middle ml-1" />
-    </a>
+    <span class="text-sm text-gray-500">... and {booksOverflow} more</span>
   {/if}
 </div>
--- src/lib/components/books/Shelves.svelte
+++ src/lib/components/books/Shelves.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { PUBLIC_GOODREADS_USER_ID } from "$env/static/public";
+
   import Section from "../Section.svelte";
   import Shelf from "./Shelf.svelte";
 
@@ -11,4 +13,16 @@
       <Shelf {...shelf} />
     {/each}
   </div>
+  <p class="text-sm text-gray-500 mt-16">
+    Data retrieved using the Goodreads API,
+    <a
+      href={`https://www.goodreads.com/user/show/${PUBLIC_GOODREADS_USER_ID}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      class="underline underline-offset-2 hover:text-indigo-600 dark:hover:text-indigo-500"
+    >
+      visit my profile
+    </a>
+    to see more.
+  </p>
 </Section>
