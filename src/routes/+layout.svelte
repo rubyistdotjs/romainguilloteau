57a783c7b96520d6b8a7e043aea63a97d08e8c58
rubyistdotjs <rubyistdotjs@gmail.com>
2023-05-17 11:50:21 +0200

    Add Abla analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+  import { PUBLIC_ABLA_WEBSITE_ID } from "$env/static/public";
   import Header from "$lib/components/header/Header.svelte";
 
   import "../app.css";
 </script>
+
+<svelte:head>
+  {#if PUBLIC_ABLA_WEBSITE_ID}
+    <script
+      async
+      defer
+      data-website-id={PUBLIC_ABLA_WEBSITE_ID}
+      src="https://tableau.abla.io/abla.js"
+    ></script>
+  {/if}
+</svelte:head>
 
 <main>
   <Header />
