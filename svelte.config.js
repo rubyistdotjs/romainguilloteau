56fe0bad3d2013cf87b4833c58bbe5eb0dcc2a1d
rubyistdotjs <rubyistdotjs@gmail.com>
2023-06-18 17:43:14 +0200

    Add CSP

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,22 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess({ preserve: ["ld+json"] })],
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    csp: {
+      directives: {
+        "default-src": ["self"],
+        "script-src": [
+          "self",
+          "sha256-+TaXqbaR7/hcKtLGLM1esquzVwTrvPtpUKNMXIiO58M=",
+          "https://tableau.abla.io/abla.js",
+        ],
+        "img-src": ["self", "data:", "https://*.gr-assets.com/"],
+        "font-src": ["https://fonts.gstatic.com/"],
+        "frame-ancestors": ["none"],
+      },
+    },
+  },
 };
 
 export default config;
